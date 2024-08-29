5f058f977b8124ee6a50555043ba846ee1fcc600
Cedric Chia <cedricchia01@gmail.com>
2024-08-30 05:22:54 +0800

    set up basic project structure
    set up basic project structure

---
+++ frontend/llm/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import "@mantine/core/styles.css";
+import React from "react";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { theme } from "../../theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const metadata = {
+  title: "Mantine Next.js template",
+  description: "I am using Mantine with Next.js!",
+};
+
+const queryClient = new QueryClient();
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en">
+      <QueryClientProvider client={queryClient}>
+        <head>
+          <ColorSchemeScript />
+          <link rel="shortcut icon" href="/favicon.svg" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+          />
+        </head>
+        <body>
+          <MantineProvider theme={theme}>{children}</MantineProvider>
+        </body>
+      </QueryClientProvider>
+    </html>
+  );
+}
---
+++ frontend/llm/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button variant="filled">Test Mantine Button</Button>
+    </main>
+  );
+}
